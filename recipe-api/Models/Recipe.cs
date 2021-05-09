c41fbf0a728eea27955fd668e2317e03cd6bf451
Allan Wu <allan.wu6@gmail.com>
2021-05-09 19:43:56 +1000

    Added Recipe Data Model

---
+++ recipe-api/Models/Recipe.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace recipe_api.Models
+{
+    public class Recipe
+    {
+        // Unique Identifier
+        public Guid id { get; set; }
+
+        // Recipe Title
+        public string title { get; set; }
+
+        // Recipe Description
+        public string description { get; set; }
+
+        // Recipe Ingredients
+        public string[] ingredients { get; set; }
+
+        // Recipe Cooking Instructions
+        public string[] method { get; set; }
+
+        // Recipe Image
+        public string image { get; set; }
+    }
+}
