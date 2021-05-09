42512c4cfd175971cb9a2c2ba45801d4bd620adc
Allan Wu <allan.wu6@gmail.com>
2021-05-09 21:06:07 +1000

    Added Database Context

---
+++ recipe-api/Controllers/RecipeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace recipe_api.Controllers
+{
+    public class RecipeController : ControllerBase
+    {
+        
+    }
+}
---
+++ recipe-api/Models/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace recipe_api.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
